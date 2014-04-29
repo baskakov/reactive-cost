494cb9a39a57d2ac7cd0a613191f060e96ac3886
OpenShift guest <saprahan@gmail.com>
2014-04-29 08:57:59 -0400

    tmp

--- app/model/AlexaActor.scala
+++ app/model/AlexaActor.scala
@@ -9,6 +9,7 @@ import play.api.libs.ws._
 import scala.concurrent.Future
 import akka.pattern.pipe
 import scala.concurrent.ExecutionContext.Implicits.global
+import play.api.libs.json.Reads._
 
 class AlexaActor extends Actor {
     val AlexaTimeout = 10000
@@ -19,13 +20,18 @@ class AlexaActor extends Actor {
         case AlexaRequest(url) => {
             val holder = WS.url("http://tools.mercenie.com/alexa-rank-checker/api/?format=json&urls=http://"+url).withRequestTimeout(AlexaTimeout)
             holder.get().map({
-              response => 
-                log.info(response.json.toString)
-                log.info(response.json \ "alexaranks" toString)
-                log.info(response.json \ "alexaranks" \ "first" toString)
-                log.info(response.json \ "alexaranks" \ "first" \ "alexarank" toString)
-                log.info(response.json \ "alexaranks" \ "first" \ "alexarank" \ "0" toString)
-                (response.json \ "alexaranks" \ "first" \ "alexarank" \ "0").as[Int]
+              response => {
+              //TODO
+                  val result = (response.json \ "alexaranks" \ "first" \ "alexarank" \ "0").asOpt[String].getOrElse("-1")
+                  log.info("A "+result)
+                  val resultInt = try {
+                    result.toInt
+                  } catch {
+                    case _ : Throwable => -1
+                  }
+                    log.info("alexa " +resultInt.toString)
+                    resultInt
+              }
             }).map(rank => AlexaResult(url, rank)).pipeTo(context.parent)
         }
     }
@@ -33,7 +39,7 @@ class AlexaActor extends Actor {
 
 case class AlexaRequest(url: String)
 
-case class AlexaResult(url: String, rank: Int) extends ResultPartValue {
+case class AlexaResult(url: String, rank: Int) extends ParsebleResultPartValue {
     val partId = AlexaPartId
     val content = rank
 }
